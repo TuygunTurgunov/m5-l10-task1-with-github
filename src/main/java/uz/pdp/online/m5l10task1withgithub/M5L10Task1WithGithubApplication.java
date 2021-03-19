28b72e7293fa6946f0343fce3608b5ab2035c9b9
Tuygun <tuygun.turgunov@gmail.com>
2021-03-19 15:39:10 +0500

    Start project

---
+++ src/main/java/uz/pdp/online/m5l10task1withgithub/M5L10Task1WithGithubApplication.java
@@ -0,0 +1,13 @@
+package uz.pdp.online.m5l10task1withgithub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class M5L10Task1WithGithubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(M5L10Task1WithGithubApplication.class, args);
+    }
+
+}
---
+++ src/test/java/uz/pdp/online/m5l10task1withgithub/M5L10Task1WithGithubApplicationTests.java
@@ -0,0 +1,13 @@
+package uz.pdp.online.m5l10task1withgithub;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class M5L10Task1WithGithubApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
